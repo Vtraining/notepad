b4a1a400c2369f390aa62ae3f0aeff9c9e382804
Vtraining <40.s.a.m.y.40@gmail.com>
2023-05-21 22:33:00 +0300

    all classes created

--- link.rb
+++ link.rb
@@ -8,9 +8,17 @@ class Link < Post
   end
 
   def read_from_console
+    puts "Enter adress of link:"
+    @url = STDIN.gets.chomp
+
+    puts "What is the link about?"
+    @text = STDIN.gets.chomp
   end
 
   def to_strings
+    time_string = "Created: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+    retun [@url, @text, time_string]
+
   end
 
 end
--- task.rb
+++ task.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Task < Post
   def innitialize
     super
@@ -7,9 +9,22 @@ class Task < Post
   end
 
   def read_from_console
+    puts "What need to do?"
+    @text = STDIN.gets.chomp
+
+    puts "Print deadline date. Put the date in DD.MM.YYYY format, for example 12.05.2003"
+    input = STDIN.gets.chomp
+
+    @due_date = Date.parse(input)
   end
   
   def to_strings
+  time_string = "Created: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+
+  deadline = "Deadline is: #{due_date}"
+
+  return [deadline, @text, time_string]
+
   end
 
 end
